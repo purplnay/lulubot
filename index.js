f3b1cfc8642340a82f483e4b821a71089292a7d6
Nay <emilydevoos@gmail.com>
2019-12-17 02:09:37 +0100

    Add bot code

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const { LuluBot } = require('./bot');
+const credentials = require('./credentials.json');
+const{removeMentions}=require('./bot').utils;
+const client = new Discord.Client();
+const lulu = new LuluBot();
+
+client.on('ready', () => {
+  console.log(`Connected as ${client.user.tag}.`);
+});
+
+client.on('message', message => {
+  console.log(message.author.bot, !message.isMentioned(client.user), message.channel.type !== 'dm')
+  if (message.author.bot) return;
+  if (!message.isMentioned(client.user) && message.channel.type !== 'dm') return;
+
+  // Reply if message author is not bot and if lulubot is mentionned or in dm channel.
+  message.reply(lulu.reply(message.content));
+});
+
+// Save corpus every 10 minutes
+setInterval(() => {
+  lulu.corpus.saveCorpus().then(() => {
+    console.log('Success saving corpus.');
+  }).catch(e => console.error('Failed saving corpus:', e.message));
+}, 600_000);
+
+client.login(credentials.token);
